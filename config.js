d94ed801400a5d8b3f435b716ae90f72419418c6
tylerhalsteadswfl-gif <tylerhalsteadswfl@gmail.com>
2026-09-11 09:08:57 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// 909CO backend settings
+// Replace these two values with your Supabase project values.
+// Do NOT put a Supabase service_role key here. Only the public anon key belongs on a website.
+window.SUPABASE_URL = 'PASTE_YOUR_SUPABASE_PROJECT_URL_HERE';
+window.SUPABASE_ANON_KEY = 'PASTE_YOUR_SUPABASE_ANON_KEY_HERE';
