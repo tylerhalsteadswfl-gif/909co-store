b4448792707f3e1939a7fdb450e0e04a4fbc657d
tylerhalsteadswfl-gif <tylerhalsteadswfl@gmail.com>
2026-09-21 08:07:44 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// 909CO GOOGLE ANALYTICS CONFIG
-window.GA_MEASUREMENT_ID = 'G-J9KWC4L4HZ';
-window.GA_PROPERTY_ID = '553886139';
-window.GOOGLE_CLIENT_ID = '93122236148-kpj6cfti1bl2v40e1ovgbpf6qpfshl9t.apps.googleusercontent.com';
+window.GA_MEASUREMENT_ID='G-J9KWC4L4HZ';
+window.GA_PROPERTY_ID='553886139';
+window.GOOGLE_CLIENT_ID='93122236148-kpj6cfti1bl2v40e1ovgbpf6qpfshl9t.apps.googleusercontent.com';
